2a9ba266a881e0b98a36c55e0d5c8ec3446d893e
allison <allison_17@hotmail.com>
2018-05-07 14:06:13 -0700

    Updating recipe blog entry with nav and organization

--- src/app/components/recipes/recipes.service.ts
+++ src/app/components/recipes/recipes.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
-        {id: 1, src: 'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/geyser-cover.jpg?raw=true'},
+        {id: 1, src: 'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/recipes/beet-muffin-cover.jpg?raw=true'},
     ];
 
     getRecipes() {
