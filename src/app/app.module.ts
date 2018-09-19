b3583563361b513727571d56144a1b2b54534571
allison <allison_17@hotmail.com>
2018-09-18 18:31:07 -0700

    first build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { AboutMeComponent } from './components/about/about-me.component';
-import { RecipesMethodComponent } from './components/recipes/method/recipes-method.component';
-import { RecipesComponent } from './components/recipes/recipes.component';
-import { TravelAlbumComponent } from './components/travel/travel-album/travel-album.component';
-import { TravelPhotosComponent } from './components/travel-photos/travel-photos.component';
-import { TravelComponent } from './components/travel/travel.component';
-import { LandingComponent } from './components/landing/landing.component';
+// import { AboutMeComponent } from './components/about/about-me.component';
+// import { RecipesMethodComponent } from './components/recipes/method/recipes-method.component';
+// import { RecipesComponent } from './components/recipes/recipes.component';
+// import { TravelAlbumComponent } from './components/travel/travel-album/travel-album.component';
+// import { TravelPhotosComponent } from './components/travel-photos/travel-photos.component';
+// import { TravelComponent } from './components/travel/travel.component';
+// import { LandingComponent } from './components/landing/landing.component';
 import { RoutingModule } from './page-routes.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -15,13 +15,13 @@ import { MatCardModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
-    TravelComponent,
-    TravelPhotosComponent,
-    TravelAlbumComponent,
-    RecipesComponent,
-    RecipesMethodComponent,
-    AboutMeComponent
+    // LandingComponent,
+    // TravelComponent,
+    // TravelPhotosComponent,
+    // TravelAlbumComponent,
+    // RecipesComponent,
+    // RecipesMethodComponent,
+    // AboutMeComponent
   ],
   imports: [
     BrowserModule,
