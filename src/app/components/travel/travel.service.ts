7e65451b746ee2843eabd5362620987b850f8d35
allison <allison_17@hotmail.com>
2018-10-10 11:36:06 -0700

    Adding Sweden photos

--- src/app/components/travel/travel.service.ts
+++ src/app/components/travel/travel.service.ts
@@ -8,23 +8,39 @@ export class TravelService {
     {id: 1, name: 'Iceland', src:
     'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/geyser-cover.jpg?raw=true',
      photo: [
-       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/beach-square.jpg?raw=true'
-
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/gullfoss.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/column.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/dynjandi.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/blue-geyser.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/dynjandi-view.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/posing.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/geysir.jpg?raw=true',
-      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/vik.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/beach-square.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/gullfoss.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/column.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/dynjandi.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/geyser-cover.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/dynjandi-view.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/posing.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/geysir.jpg?raw=true',
+       'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/vik.jpg?raw=true',
     ]
     },
     {id: 2, name: 'Scotland', src:
-    'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/scotland/cover.jpg'},
+    'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/scotland/cover.jpg',
+    photo: [
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/royal-mile.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/view-from-tantallon.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/edinburgh-castle.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/ruins.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/view-edinburgh.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/cover.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/church.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/me-tantallon.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/scotland/tantallon.jpg?raw=true',
+
+    ]
+  },
     {id: 3, name: 'Sweden', src:
     'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/lulea/sunken-ship-cover%20(3).jpg',
-      photo: ['https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/lulea/walking-bothnia.jpg?raw=true']}
+    photo: [
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/lulea/walking-bothnia.jpg?raw=true',
+      'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/lulea/trail.jpg?raw=true'
+    ]
+    },
 ];
 
 constructor() {
