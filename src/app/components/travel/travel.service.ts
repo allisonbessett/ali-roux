1aff403029c9146d44e6fd2ab0c1ea89f42ed4ff
allison <allison_17@hotmail.com>
2018-10-10 10:39:03 -0700

    Adding square photo

--- src/app/components/travel/travel.service.ts
+++ src/app/components/travel/travel.service.ts
@@ -8,8 +8,16 @@ export class TravelService {
     {id: 1, name: 'Iceland', src:
     'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/geyser-cover.jpg?raw=true',
      photo: [
-       'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/iceland/beach.jpg',
-     'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/scotland/cover.jpg'
+       'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/iceland/beach.jpg'
+      //  'https://raw.githubusercontent.com/allisonbessett/ali-roux/master/src/app/images/iceland/beach.jpg',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/gullfoss.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/column.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/Iceland/dynjandi.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/blue-geyser.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/dynjandi-view.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/posing.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/geysir.jpg?raw=true',
+      //  'https://github.com/allisonbessett/ali-roux/blob/master/src/app/images/iceland/vik.jpg?raw=true',
     ]
     },
     {id: 2, name: 'Scotland', src:
